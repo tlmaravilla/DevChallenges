8e9b0f3d3bd158e386d7bd907bf4363a08b375cf
Tom Maravilla <tlmaravilla@gmail.com>
2020-05-28 12:13:20 -0600

    SameCaseAnswer

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace DevChallenges
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Dev Challenge in 3 2 1, GO!");
+
+         var result = CheckIfSameCase("HeLLO");
+         System.Console.WriteLine($"CheckSameCase: {result}");
+
+      }
+
+
+      public static bool CheckIfSameCase(string msg)
+      {
+         if (msg.Equals(msg.ToUpper()))
+            return true;
+
+         if (msg.Equals(msg.ToLower()))
+            return true;
+
+         return false;
+      }
+   }
+}
