2b14bb9514ff4a8352903019126e8cc4fe90139c
Tom Maravilla <tlmaravilla@churchofjesuschrist.org>
2021-04-19 12:55:58 -0600

    stream lined reverse case

--- Challenges/MediumChallenges.cs
+++ Challenges/MediumChallenges.cs
@@ -1,26 +1,32 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace DevChallenges.Challenges
 {
     public class MediumChallenges
     {
-        public static string ReverseTheCase(string msg)
-        {
-            var bldr = new StringBuilder();
-            foreach (var item in msg.ToCharArray())
-            {
-                if (char.IsWhiteSpace(item))
-                {
-                    bldr.Append(item);
-                    continue;
-                }
+        public static string ReverseTheCase(string str) =>
 
-                // Use Ternary conditional operator
-                var itemRevCase = char.IsUpper(item) ? item.ToString().ToLower() : item.ToString().ToUpper();
-                bldr.Append(itemRevCase);
-            }
+            // First Try, lets stream line a bit with linq
 
-            return bldr.ToString();
-        }
+            //var bldr = new StringBuilder();
+            //foreach (var item in msg.ToCharArray())
+            //{
+            //    if (char.IsWhiteSpace(item))
+            //    {
+            //        bldr.Append(item);
+            //        continue;
+            //    }
+
+            //    // Use Ternary conditional operator
+            //    var itemRevCase = char.IsUpper(item) ? item.ToString().ToLower() : item.ToString().ToUpper();
+            //    bldr.Append(itemRevCase);
+            //}
+
+            //return bldr.ToString();
+
+            new string(str.ToCharArray().Select(
+                s => char.IsLower(s) ? char.ToUpper(s) : char.ToLower(s)).ToArray());
     }
 }
